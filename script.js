98281965476beeeea7d4aaf7bf850072210f39dc
ArpanSankesh <arpansankesh830@gmail.com>
2023-11-29 14:39:46 +0530

    added some features and fixed the bug

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 const buttons = document.querySelectorAll(".btn");
 const AC = document.querySelector(".clear");
 const equalbtn = document.querySelector(".equalBtn");
 const input = document.querySelector("#input");
 const backSpaceBtn = document.querySelector(".backSpaceBtn");
+const history = document.querySelector('#history')
 
 let string = "";
 
@@ -11,13 +11,14 @@ AC.addEventListener("click", () => {
   input.style.fontSize = "5.5rem";
   string = "";
   input.value = string;
+  history.innerHTML = string;
 });
 
-backSpaceBtn.addEventListener("click", ()=>{
-  if (string.length = 0) {
+backSpaceBtn.addEventListener("click", () => {
+  if ((string.length = 0)) {
     input.style.fontSize = "5.5rem";
-  } 
-  string = string.slice(0,string.length -1);
+  }
+  string = string.slice(0, string.length - 1);
   input.value = string;
 });
 
@@ -26,7 +27,6 @@ buttons.forEach(function (button) {
     string = string + e.target.innerHTML;
     console.log(string);
     input.value = string;
-
 
     if (string.length >= 9) {
       input.style.fontSize = "2rem";
@@ -39,14 +39,8 @@ buttons.forEach(function (button) {
 equalbtn.addEventListener("click", () => {
   string = eval(string);
   input.value = string;
+  history.innerHTML = string;
   console.log(string);
+  string = '';
 });
-
-
-document.addEventListener('mousemove', function(e){
-  let mouseX = e.clientX;
-  let mouseY = e.clientY;
-
-  // console.log(mouseX, mouseY);
-})
 
